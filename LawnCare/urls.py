e4a78629378dbdd5ee82136a6b71f3aea9123265
B2the52 <112206151+B2the52@users.noreply.github.com>
2023-10-24 21:46:02 -0500

    added

---
+++ LawnCare/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, re_path
+from . import views
+from django.views.static import serve
+from django.conf import settings
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('blog/', views.blog, name='blog'),
+    path('service_list/', views.ServiceListView.as_view(), name='service_list'),
+    path('service_detail/<int:pk>', views.ServiceDetailView.as_view(), name='service_detail'),
+    path('service/create/', views.ServiceCreate.as_view(), name='service_create'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
+]
